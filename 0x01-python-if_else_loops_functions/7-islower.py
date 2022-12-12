f58976bb84f4cbb9c5072c436398dfce1e7f1664
nkweku66 <nkweku66@gmail.com>
2022-12-11 16:44:35 -0800

    programming files

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    for char in c:
-        if(ord(char) >=97 and ord(char) <=122):
-            print("True")
-        else:
-            print("False")
+    if(ord(char) >=97 and ord(char) <=122):
+        print("True")
+    else:
+        print("False")
 
