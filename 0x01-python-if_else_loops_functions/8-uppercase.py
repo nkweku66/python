ff079f9c5a867861370c0baacab9d90c8b5ff3db
nkweku66 <nkweku66@gmail.com>
2022-12-11 18:47:40 -0800

    programming files

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for string in str:
+        if ord(string) >= 97 and ord(string) <= 122:
+            string = chr(ord(string) - 32)
+        print("{}".format(string), end="")
+    print("")
