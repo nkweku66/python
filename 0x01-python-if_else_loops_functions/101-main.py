9c32d1ed27b2adb6db8d5ce4dfb90620dfceaf15
nkweku66 <nkweku66@gmail.com>
2023-01-22 11:42:37 -0800

    programming files

--- 0x01-python-if_else_loops_functions/101-main.py
+++ 0x01-python-if_else_loops_functions/101-main.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 remove_char_at = __import__('101-remove_char_at').remove_char_at
 print(remove_char_at("best school", 3))
+print(remove_char_at("chicago", 15))
+print(remove_char_at("hello", -4))
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return ("{}".format(str.replace(str[n], '')))
+    if n >= len(str) or n < 0:
+        return str
+    else:
+        return ("{}".format(str.replace(str[n], '')))
