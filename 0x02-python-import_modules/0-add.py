19eb79a406f18dd792feb950b220bc0bb8898ecd
nkweku66 <nkweku66@gmail.com>
2023-02-06 08:49:35 -0800

    programming files

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print sum of 1 and 2"""
     from add_0 import add
-    """Print sum of a and b"""
+ 
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a,b)))
